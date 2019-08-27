7382a55e232a0eeb8c2118493c78aa5b10f2f378
corbinarnett <corbin.c.arnett@gmail.com>
2019-08-26 17:03:58 -0700

    added destinations

--- lib/travel_leisure/cli.rb
+++ lib/travel_leisure/cli.rb
@@ -6,10 +6,15 @@ class TravelLeisure::CLI #:: name space
       # get_user_destination
       #display_destination_info(user_input)
     end
-
-    def list_destinations
-      # to be scraped instead
+      
+    def list_destinations_with_index
+      #to be scraped instead
       @destinations = ["Amsterdam, Netherlands", "Bali, Thailand", "Austin, Texas"] #@destinations is an instance variable
+      
+      @destinations.each_with_index do |item, index|
+        puts "#{index}. #{item}"
+      end
+      
       
     end
 
