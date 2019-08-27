b4e12f795e9226fc81347c54238f5c8c4f462cdd
corbinarnett <corbin.c.arnett@gmail.com>
2019-08-26 22:06:48 -0700

    added method for to display chosen output

--- lib/travel_leisure/cli.rb
+++ lib/travel_leisure/cli.rb
@@ -22,6 +22,21 @@ class TravelLeisure::CLI #:: name space
 
     def get_user_destination
       chosen_destination = gets.strip.to_i
+      more_info_for(chosen_destination) if valid_input(chosen_destination, @destinations)
+    end
+
+    def valid_input(input, data)
+      input.to_i <= data.length && input.to_i > 0
+    end
+
+    def more_info_for(chosen_destination)
+      destination = @months[chosen_destination - 1]
+      puts "Here is more info for #{destination}:"
+      ##To implement
+      # TravelLeisure::Destination.all.each.with_index(1) do | destination |
+      #   puts destination.name
+      # end
+      # get_user_destination
     end
 
 end
