57c6b920d08cccbb593b0cb731d6c07e6b8fd8ce
corbinarnett <corbin.c.arnett@gmail.com>
2019-08-28 00:20:35 -0700

    added new formatting to cli

--- lib/travel_leisure/cli.rb
+++ lib/travel_leisure/cli.rb
@@ -2,39 +2,69 @@
 class TravelLeisure::CLI #:: name space
     def call
       TravelLeisure::Scraper.new.make_destinations
-      puts "\nWelcome to Command Line Travel Guides!"
+      puts ""
+      puts ""
+      puts " ------------------------------------------------ "
+      puts "|                                                |"
+      puts "|    Welcome to Command Line Travel Guides       |"
+      puts "|                                                |"
+      puts " ------------------------------------------------ "
+      puts ""
+      puts ""
+
       menu
     end
 
     def menu
-      input = ""
-      while input != "exit"
-      puts ""
-      print_destinations
-      puts "\nPlease enter the number of the travel guide you would like to access:"
-      input = gets.strip.to_i
-
-      destination = TravelLeisure::Destination.find(input)
-
-      print_destination(destination)
-      
-      puts ""
-      puts "Would you like to see another travel guide? Enter Y or N"
-
-      input = gets.strip.downcase
-      if input == "y"
-        menu
-      elsif input == "n"
+        puts "To view the destinations that we have travel guides for, enter 'travel'."
+        puts "or enter 'q' to exit"
+        input = gets.strip.downcase
+        if input == 'travel'
+          puts ""
+        puts "Here is a list of destinations that we have travel guides for:"
         puts ""
-        puts "Thank you! Have a great day!"
-        exit
-      else
-        puts ""
-        puts "I don't understand that answer."
-        menu
-      end
+        print_destinations
+        elsif input == "q"
+          exit
+        else
+          puts ""
+          puts "I don't understand that answer, please try again:"
+          puts ""
+          menu
+        end
+        # puts ""
+        # puts "Here is a list of destinations that we have travel guides for:"
+        # puts ""
+        # print_destinations
+
+
+        puts "\nPlease enter the number of the travel guide you would like to access\nor enter 'q' to exit:"
+        input = gets.strip
+        if input == 'q'
+          exit
+        elsif
+          destination = TravelLeisure::Destination.find(input.to_i)
+          print_destination(destination)
+        end
+        # destination = TravelLeisure::Destination.find(input.to_i)
+
+        # print_destination(destination)
       
-      end
+        puts ""
+        puts "Would you like to see another travel guide? Enter Y or N"
+
+        input = gets.strip.downcase
+        if input == "y"
+          menu
+        elsif input == "n"
+          puts ""
+          puts "Thank you! Have a great day!"
+          exit
+        else
+          puts ""
+          puts "I don't understand that answer."
+          menu
+        end
     end
 
     def print_destination(destination)
@@ -52,9 +82,14 @@ class TravelLeisure::CLI #:: name space
 
         input = gets.strip.downcase
         if input == "y"
+          puts ""
+          puts "More info:"
+          puts ""
           puts destination.description
         elsif input == "n"
           menu
+        elsif input == "q"
+          exit
         else
           puts ""
           puts "I don't understand that answer."
