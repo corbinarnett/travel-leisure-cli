d282c53eb9f1522a9e4bd13c0744e7e17831ed33
corbinarnett <corbin.c.arnett@gmail.com>
2019-08-28 01:06:19 -0700

    added colorize to files

--- lib/travel_leisure/cli.rb
+++ lib/travel_leisure/cli.rb
@@ -4,11 +4,11 @@ class TravelLeisure::CLI #:: name space
       TravelLeisure::Scraper.new.make_destinations
       puts ""
       puts ""
-      puts " ------------------------------------------------ "
-      puts "|                                                |"
-      puts "|    Welcome to Command Line Travel Guides       |"
-      puts "|                                                |"
-      puts " ------------------------------------------------ "
+      puts " ------------------------------------------------ ".blue
+      puts "|                                                |".blue
+      puts "|    Welcome to Command Line Travel Guides       |".blue.bold
+      puts "|                                                |".blue
+      puts " ------------------------------------------------ ".blue
       puts ""
       puts ""
 
@@ -16,19 +16,19 @@ class TravelLeisure::CLI #:: name space
     end
 
     def menu
-        puts "To view the destinations that we have travel guides for, enter 'travel'."
-        puts "or enter 'q' to exit"
+        puts "To view the destinations that we have travel guides for, enter 'travel'.".yellow.bold
+        puts "or enter 'q' to exit".yellow.bold
         input = gets.strip.downcase
         if input == 'travel'
           puts ""
-        puts "Here is a list of destinations that we have travel guides for:"
+        puts "Here is a list of destinations that we have travel guides for:".yellow.bold
         puts ""
         print_destinations
         elsif input == "q"
           exit
         else
           puts ""
-          puts "I don't understand that answer, please try again:"
+          puts "I don't understand that answer, please try again:".red.bold
           puts ""
           menu
         end
@@ -38,47 +38,48 @@ class TravelLeisure::CLI #:: name space
         # print_destinations
 
 
-        puts "\nPlease enter the number of the travel guide you would like to access\nor enter 'q' to exit:"
+        puts "\nPlease enter the number of the travel guide you would like to access\nor enter 'q' to exit:".yellow.bold
         input = gets.strip
         if input == 'q'
           exit
         elsif
           destination = TravelLeisure::Destination.find(input.to_i)
           print_destination(destination)
+        
         end
         # destination = TravelLeisure::Destination.find(input.to_i)
 
         # print_destination(destination)
       
         puts ""
-        puts "Would you like to see another travel guide? Enter Y or N"
+        puts "Would you like to see another travel guide? Enter Y or N".yellow.bold
 
         input = gets.strip.downcase
         if input == "y"
           menu
         elsif input == "n"
           puts ""
-          puts "Thank you! Have a great day!"
+          puts "Thank you! Have a great day!".yellow.bold
           exit
         else
           puts ""
-          puts "I don't understand that answer."
+          puts "I don't understand that answer.".red.bold
           menu
         end
     end
 
     def print_destination(destination)
       puts ""
-      puts "----------- #{destination.city}, #{destination.country} -----------"
+      puts "----------- #{destination.city}, #{destination.country} -----------".blue.bold
       puts ""
-      puts "Best Time To Visit: #{destination.best_time_to_visit}"
-      puts "\nTransportation: #{destination.transportation}"
-      puts "\nWeather: #{destination.weather}"
-      puts "\nKnow Before Visiting: #{destination.know_before_visiting}"
-      puts "\nLanguage: #{destination.language}"
-      puts "\nCurrency: #{destination.currency}"
+      puts "Best Time To Visit: #{destination.best_time_to_visit}".green.bold
+      puts "\nTransportation: #{destination.transportation}".green.bold
+      puts "\nWeather: #{destination.weather}".green.bold
+      puts "\nKnow Before Visiting: #{destination.know_before_visiting}".green.bold
+      puts "\nLanguage: #{destination.language}".green.bold
+      puts "\nCurrency: #{destination.currency}".green.bold
       puts ""
-      puts "Would you like to read more on #{destination.city}? Enter Y or N"
+      puts "Would you like to read more on #{destination.city}? Enter Y or N".yellow.bold
 
         input = gets.strip.downcase
         if input == "y"
